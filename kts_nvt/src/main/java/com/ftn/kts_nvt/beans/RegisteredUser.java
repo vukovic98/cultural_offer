78354d08e1065474bf90161f997c21b6afea09a3
ivanavlaisavljevic <ivanazvucnik@gmail.com>
2020-11-22 12:00:44 +0100

    anotations added

--- kts_nvt/src/main/java/com/ftn/kts_nvt/beans/RegisteredUser.java
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/beans/RegisteredUser.java
@@ -1,5 +1,34 @@
 package com.ftn.kts_nvt.beans;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "registeredUser")
 public class RegisteredUser extends User {
- 
+	private Collection<Comment> comments;
+	private Collection<CulturalOffer> culturalOffers;
+	
+	public RegisteredUser() {
+		
+	}
+	public RegisteredUser(Collection<Comment> comments, Collection<CulturalOffer> culturalOffers) {
+		super();
+		this.comments = comments;
+		this.culturalOffers = culturalOffers;
+	}
+	public Collection<Comment> getComments() {
+		return comments;
+	}
+	public void setComments(Collection<Comment> comments) {
+		this.comments = comments;
+	}
+	public Collection<CulturalOffer> getCulturalOffers() {
+		return culturalOffers;
+	}
+	public void setCulturalOffers(Collection<CulturalOffer> culturalOffers) {
+		this.culturalOffers = culturalOffers;
+	}
 }
