35141aeabc3da8bb3cd431f923c4cf142661986c
Vladimir Vukovic <vladimirvukovic98@hotmail.rs>
2020-11-22 10:21:38 +0100

    Initial commit

---
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/KtsNvtApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.kts_nvt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtsNvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtsNvtApplication.class, args);
+	}
+
+}
---
+++ kts_nvt/src/test/java/com/ftn/kts_nvt/KtsNvtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.kts_nvt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtsNvtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
