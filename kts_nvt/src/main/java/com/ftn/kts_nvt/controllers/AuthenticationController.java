262d8ba92cdc234c814c65315ee843504cc18b77
alen1010 <42214173+alen1010@users.noreply.github.com>
2020-11-25 21:07:53 +0100

    AuthenticationController

---
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/controllers/AuthenticationController.java
@@ -0,0 +1,11 @@
+package com.ftn.kts_nvt.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authentication")
+public class AuthenticationController {
+
+}
