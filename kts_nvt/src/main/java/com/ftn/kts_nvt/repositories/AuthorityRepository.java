728daf11abe934e7dfb500319175c829cc1f469b
alen1010 <alenmujo10@gmail.com>
2020-11-25 20:43:02 +0100

    Authority

---
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/repositories/AuthorityRepository.java
@@ -0,0 +1,12 @@
+package com.ftn.kts_nvt.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ftn.kts_nvt.beans.Authority;
+
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+
+    Authority findByName(String name);
+}
