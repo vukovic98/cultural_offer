743f1a3f7bf2add8449d9e232e6f8eab6b2edd6b
alen1010 <42214173+alen1010@users.noreply.github.com>
2020-11-25 21:06:39 +0100

    Add files via upload
    Authority

---
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/beans/Authority.java
@@ -0,0 +1,45 @@
+package com.ftn.kts_nvt.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+
+@Entity
+@Table(name="authority")
+public class Authority implements GrantedAuthority {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name="name")
+    String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
