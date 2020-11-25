3eae938add82dce1038fe2ea5d923c7c4f4c6687
dusanmadzarevic <ducom98@gmail.com>
2020-11-25 21:38:13 +0100

    User class fix

--- kts_nvt/src/main/java/com/ftn/kts_nvt/beans/User.java
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/beans/User.java
@@ -1,37 +1,50 @@
 package com.ftn.kts_nvt.beans;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import javax.persistence.JoinColumn;
 
 @Entity
 @Embeddable
 @Table(name = "user")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.TABLE)
 	@Column(name = "user_id")
 	private Long id;
-	
+
 	@Column(name = "firstName", nullable = false)
 	private String firstName;
-	
+
 	@Column(name = "lastName", nullable = false)
 	private String lastName;
-	
+
 	@Column(name = "email", nullable = false)
-    private	String email;
-	
+	private String email;
+
 	@Column(name = "password", nullable = false)
 	private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_authority",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
+    private List<Authority> authorities;
 
 	public User() {
 
@@ -83,6 +96,14 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Authority> getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(List<Authority> authorities) {
+		this.authorities = authorities;
 	}
 
 }
