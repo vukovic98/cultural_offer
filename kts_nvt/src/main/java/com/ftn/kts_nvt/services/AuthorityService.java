a8eb238cafbd01925fd96fd3c6114e426f36f72a
alen1010 <42214173+alen1010@users.noreply.github.com>
2020-11-25 21:09:31 +0100

    AuthorityService

---
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/services/AuthorityService.java
@@ -0,0 +1,31 @@
+package com.ftn.kts_nvt.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ftn.kts_nvt.beans.Authority;
+import com.ftn.kts_nvt.repositories.AuthorityRepository;
+
+@Service
+public class AuthorityService {
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+    public List<Authority> findById(Long id) {
+        Authority auth = this.authorityRepository.getOne(id);
+        List<Authority> auths = new ArrayList<>();
+        auths.add(auth);
+        return auths;
+    }
+
+    public List<Authority> findByName(String name) {
+        Authority auth = this.authorityRepository.findByName(name);
+        List<Authority> auths = new ArrayList<>();
+        auths.add(auth);
+        return auths;
+    }
+}
