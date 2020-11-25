da7ae8d601cf1a001243abd6a15f81ba67ce17ec
alen1010 <alenmujo10@gmail.com>
2020-11-25 21:15:26 +0100

    test

--- kts_nvt/src/main/java/com/ftn/kts_nvt/beans/WorkingHour.java
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/beans/WorkingHour.java
@@ -12,7 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+//a
 @Entity
 @Table(name = "working_hour")
 @Embeddable
