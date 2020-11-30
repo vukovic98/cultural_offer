09ce4b9d99c244dadd3c4d611cbb9239fc268816
ivanavlaisavljevic <ivanazvucnik@gmail.com>
2020-11-30 09:21:24 +0100

    created: GradeController.java GradeRepository.java GradeService.java

---
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/repositories/GradeRepository.java
@@ -0,0 +1,11 @@
+package com.ftn.kts_nvt.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ftn.kts_nvt.beans.Grade;
+
+@Repository
+public interface GradeRepository extends JpaRepository<Grade, Long>{
+
+}
