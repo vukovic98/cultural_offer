6b999fbac6e391b518a089efd2928c34ab5333c3
Vladimir Vukovic <vladimirvukovic98@hotmail.rs>
2020-12-02 15:33:47 +0100

    bug

--- kts_nvt/src/main/java/com/ftn/kts_nvt/controllers/CulturalOfferTypeController.java
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/controllers/CulturalOfferTypeController.java
@@ -30,7 +30,6 @@ public class CulturalOfferTypeController {
 	@Autowired
 	private CulturalOfferTypeService culturalOfferTypeService;
 
-	@Autowired
 	private CulturalOfferTypeMapper mapper;
 
 	@GetMapping(path = "/byCategory/{categoryId}")
