76f299191d0aea5e5432070f7dd011aaad61e038
dusanmadzarevic <ducom98@gmail.com>
2020-12-04 19:50:01 +0100

    Added authorization header to swagger requests

--- kts_nvt/src/main/java/com/ftn/kts_nvt/SwaggerConfig.java
+++ kts_nvt/src/main/java/com/ftn/kts_nvt/SwaggerConfig.java
@@ -1,23 +1,62 @@
 package com.ftn.kts_nvt;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-
-
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class SwaggerConfig {                                    
-    @Bean
+public class SwaggerConfig {
+	@Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
+          .select()  
+          .paths(PathSelectors.any()) 
           .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+          .build()
+	      .securitySchemes(Arrays.asList(apiKey())) 
+	      .securityContexts(Arrays.asList(securityContext()));                                         
     }
+
+	@Bean
+	public SecurityConfiguration security() {
+		return SecurityConfigurationBuilder.builder().scopeSeparator(",").additionalQueryStringParams(null)
+				.useBasicAuthenticationWithAccessCodeGrant(false).build();
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("apiKey", "Authorization", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("apiKey", authorizationScopes));
+	}
 }
