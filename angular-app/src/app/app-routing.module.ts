287748a22cf2a3b697220cc5b474a60cbdc75f9e
Vladimir Vukovic <vladimirvukovic98@hotmail.rs>
2020-12-10 17:19:03 +0100

    initial routes

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home-page', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'home-page', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
