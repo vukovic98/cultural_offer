2d70c955b65aa62c29a6e203c7ce3c00a06459a0
alen1010 <alenmujo10@gmail.com>
2020-12-22 20:47:37 +0100

    test

--- kts_nvt/src/test/java/com/ftn/kts_nvt/SuiteAll.java
+++ kts_nvt/src/test/java/com/ftn/kts_nvt/SuiteAll.java
@@ -7,9 +7,19 @@ import org.springframework.test.context.TestPropertySource;
 
 import com.ftn.kts_nvt.service.CommentServiceUnitTest;
 import com.ftn.kts_nvt.service.CulturalOfferServiceUnitTest;
+import com.ftn.kts_nvt.service.CulturalOfferTypeServiceIntegrationTest;
+import com.ftn.kts_nvt.service.CulturalOfferTypeServiceUnitTest;
+import com.ftn.kts_nvt.service.ImageServiceIntegrationTest;
+import com.ftn.kts_nvt.service.ImageServiceUnitTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({CulturalOfferServiceUnitTest.class, CommentServiceUnitTest.class})
+@SuiteClasses({
+	//CulturalOfferServiceUnitTest.class,
+	//CommentServiceUnitTest.class,
+	//ImageServiceUnitTest.class,
+	//ImageServiceIntegrationTest.class,
+	CulturalOfferTypeServiceIntegrationTest.class
+})
 @TestPropertySource("classpath:test.properties")
 public class SuiteAll {
 
---
+++ kts_nvt/src/test/java/com/ftn/kts_nvt/service/CulturalOfferTypeServiceIntegrationTest.java
@@ -0,0 +1,92 @@
+package com.ftn.kts_nvt.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ftn.kts_nvt.beans.CulturalOfferType;
+import com.ftn.kts_nvt.beans.Image;
+import com.ftn.kts_nvt.services.CulturalOfferCategoryService;
+import com.ftn.kts_nvt.services.CulturalOfferTypeService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:test.properties")
+public class CulturalOfferTypeServiceIntegrationTest {
+
+	@Autowired
+	private CulturalOfferTypeService service;
+
+	@Autowired
+	private CulturalOfferCategoryService categoryService;
+	 
+	@Test
+	public void testFindAll() {
+		List<CulturalOfferType> found = service.findAll();
+		assertEquals(1, found.size());
+	}
+	
+	@Test
+    public void testFindAllPageable() {
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<CulturalOfferType> found = service.findAll(pageable);
+        assertEquals(1, found.getNumberOfElements());
+    }
+	@Test
+	public void testFindById() {
+		CulturalOfferType found = service.findById(1L);
+	    assertEquals(Long.valueOf(1), found.getId());
+	    assertEquals("Festival", found.getName());
+	}
+	
+	@Test
+	public void testFindByIdFail() {
+		CulturalOfferType found = service.findById(10L);
+	    assertEquals(null, found);
+	}
+	
+	@Test
+	public void testCreate() throws Exception {
+		CulturalOfferType type = new CulturalOfferType();
+		type.setId(2L);
+		type.setName("Museum");
+		//INSERT INTO kts_nvt_test.cultural_offer_category VALUES (2,'Institution');
+		type.setCategory(categoryService.findOne(2L));
+		CulturalOfferType created = service.save(type);
+
+	     assertEquals(type.getName(), created.getName());
+	     assertEquals(type.getId(), created.getId());
+	     assertEquals(type, created);
+	}
+	
+	@Test
+	public void testDelete() throws Exception {
+		service.deleteById(1L);
+
+	    assertEquals(1, service.findAll().size());
+	    assertEquals(null, service.findById(1L));
+	}
+	
+	@Test
+	public void testUpdate() throws Exception {
+		CulturalOfferType type = service.findById(2L);
+		type.setName("ChangedName");
+		type.setCategory(categoryService.findOne(1L));
+		
+		CulturalOfferType changed = service.update(type, 2L);
+		
+	    assertEquals("ChangedName", changed.getName());
+	    assertEquals(Long.valueOf(1L), changed.getCategory().getId());
+	    
+	}
+}
