fc1f5655f95c01113da6ef98fd24e1d407f1f624
Vladimir Vukovic <vladimirvukovic98@hotmail.rs>
2020-12-24 10:32:04 +0100

    integration tests

--- kts_nvt/src/test/java/com/ftn/kts_nvt/SuiteAll.java
+++ kts_nvt/src/test/java/com/ftn/kts_nvt/SuiteAll.java
@@ -5,19 +5,26 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.test.context.TestPropertySource;
 
+import com.ftn.kts_nvt.controller.CulturalOfferControllerIntegrationTest;
+import com.ftn.kts_nvt.service.CommentServiceUnitTest;
 import com.ftn.kts_nvt.service.CulturalOfferServiceIntegrationTest;
+import com.ftn.kts_nvt.service.CulturalOfferServiceUnitTest;
+import com.ftn.kts_nvt.service.CulturalOfferTypeServiceIntegrationTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	CulturalOfferServiceUnitTest.class,
-	CommentServiceUnitTest.class,
+	//CulturalOfferServiceUnitTest.class,
+	//CommentServiceUnitTest.class,
 	//CulturalOfferServiceUnitTest.class,
 	//CommentServiceUnitTest.class,
 	//ImageServiceIntegrationTest.class,
-	CulturalOfferTypeServiceIntegrationTest.class
+	//CulturalOfferTypeServiceIntegrationTest.class,
 	//CulturalOfferTypeServiceIntegrationTest.class,
 	//CulturalOfferRepositoryIntegrationTest.class,
-	CulturalOfferServiceIntegrationTest.class})@TestPropertySource("classpath:test.properties")
+	//CulturalOfferServiceIntegrationTest.class,
+	CulturalOfferControllerIntegrationTest.class
+})
+@TestPropertySource("classpath:test.properties")
 public class SuiteAll {
 
 
