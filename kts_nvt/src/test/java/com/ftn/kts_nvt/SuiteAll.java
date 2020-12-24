9784e27d33108fdd72d110520c18049fc249bea2
ivanavlaisavljevic <ivanazvucnik@gmail.com>
2020-12-24 11:25:13 +0100

    err

--- kts_nvt/src/test/java/com/ftn/kts_nvt/SuiteAll.java
+++ kts_nvt/src/test/java/com/ftn/kts_nvt/SuiteAll.java
@@ -5,19 +5,23 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.test.context.TestPropertySource;
 
+import com.ftn.kts_nvt.repository.CulturalOfferRepositoryIntegrationTest;
+import com.ftn.kts_nvt.service.CommentServiceUnitTest;
 import com.ftn.kts_nvt.service.CulturalOfferServiceIntegrationTest;
+import com.ftn.kts_nvt.service.CulturalOfferServiceUnitTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	CulturalOfferServiceUnitTest.class,
 	CommentServiceUnitTest.class,
-	//CulturalOfferServiceUnitTest.class,
-	//CommentServiceUnitTest.class,
+	CulturalOfferRepositoryIntegrationTest.class,
+	CulturalOfferServiceIntegrationTest.class
 	//ImageServiceIntegrationTest.class,
-	CulturalOfferTypeServiceIntegrationTest.class
+	//CulturalOfferTypeServiceIntegrationTest.class
 	//CulturalOfferTypeServiceIntegrationTest.class,
 	//CulturalOfferRepositoryIntegrationTest.class,
-	CulturalOfferServiceIntegrationTest.class})@TestPropertySource("classpath:test.properties")
+	//
+	})@TestPropertySource("classpath:test.properties")
 public class SuiteAll {
 
 
