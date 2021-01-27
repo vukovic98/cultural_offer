640b4fa46daa11e018463c30d038f7d526b19326
alen1010 <alenmujo10@gmail.com>
2021-01-27 09:52:59 +0100

    add offer coordinates

--- angular-app/src/app/components/add-offer/add-offer.component.ts
+++ angular-app/src/app/components/add-offer/add-offer.component.ts
@@ -41,7 +41,7 @@ export class AddOfferComponent implements OnInit {
 
   ngOnInit() {
     // @ts-ignore
-    let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    let mymap = L.map('mapid').setView([44.787197, 20.457273], 6);
     // @ts-ignore
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
